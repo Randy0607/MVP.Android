24f50d09d3422f14a97175c4e89a8f4d10d97bfd
Randy0607 <randall.julies89@gmail.com>
2024-03-24 16:32:32 +0200

    update

---
+++ DependencyServices/StatusBar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Android.OS;
+using MVP.Helpers;
+using Plugin.CurrentActivity;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: Dependency(typeof(MVP.Droid.DependencyServices.Statusbar))]
+namespace MVP.Droid.DependencyServices
+{
+    public class Statusbar : IStatusBar
+    {
+        public Statusbar()
+        {
+        }
+
+        public void SetStatusBarColor(OSAppTheme theme, Color overrideColor = default)
+        {
+            // The SetStatusBarcolor is new since API 21
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                var color = overrideColor == default ? theme == OSAppTheme.Dark ? (Color)Xamarin.Forms.Application.Current.Resources["black"] : (Color)Xamarin.Forms.Application.Current.Resources["white"] : overrideColor;
+                var darkStatusBarTint = theme != OSAppTheme.Dark;
+
+                var window = CrossCurrentActivity.Current.Activity.Window;
+                window.AddFlags(Android.Views.WindowManagerFlags.DrawsSystemBarBackgrounds);
+                window.ClearFlags(Android.Views.WindowManagerFlags.TranslucentStatus);
+                window.SetStatusBarColor(color.ToAndroid());
+
+                var flag = (Android.Views.StatusBarVisibility)Android.Views.SystemUiFlags.LightStatusBar;
+                window.DecorView.SystemUiVisibility = darkStatusBarTint || overrideColor != default ? flag : 0;
+            }
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Microsoft.Identity.Client;
+using Android.Content;
+using MVP.Services;
+using Plugin.CurrentActivity;
+using Acr.UserDialogs;
+using MVP.Services.Interfaces;
+using Autofac;
+
+namespace MVP.Droid
+{
+    [Activity(
+        Label = "MVPBuzz",
+        Theme = "@style/MainTheme.Launcher",
+        ConfigurationChanges = ConfigChanges.Locale | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            // Init plugins
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(enableFastRenderer: true);
+            UserDialogs.Init(this);
+
+            // Inject analytics service
+            using var scope = ContainerService.Container.BeginLifetimeScope();
+            var analyticsService = scope.Resolve<IAnalyticsService>();
+            var apiService = scope.Resolve<IMvpApiService>();
+            var authService = scope.Resolve<IAuthService>();
+            var languageService = scope.Resolve<LanguageService>();
+
+            LoadApplication(new App(analyticsService, apiService, authService, languageService));
+
+            // Set the current activity so the AuthService knows where to start.
+            MsalAuthService.ParentWindow = CrossCurrentActivity.Current.Activity;
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            AuthenticationContinuationHelper.SetAuthenticationContinuationEventArgs(requestCode, resultCode, data);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
